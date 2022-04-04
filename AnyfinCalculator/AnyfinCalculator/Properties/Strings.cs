3663c9ab640a7e03115a0b93438a78daeffa64fd
Batian Styx <batstyx@gmail.com>
2022-04-04 09:00:04 +0100

    SetttingsView initial

---
+++ AnyfinCalculator/AnyfinCalculator/Properties/Strings.cs
@@ -0,0 +1,9 @@
+﻿using WPFLocalizeExtension.Engine;
+
+namespace AnyfinCalculator.Properties
+{
+    internal class Strings
+    {
+        public static string Get(string key) => LocalizeDictionary.Instance.GetLocalizedObject(LibraryInfo.Name, "Resources", key, LocalizeDictionary.Instance.Culture)?.ToString();
+    }
+}
